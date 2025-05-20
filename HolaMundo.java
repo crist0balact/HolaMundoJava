d91984507eeefeb5044dccd82d7320403bda4898
crist0balact <cr.cisternast@duocuc.cl>
2025-05-20 12:16:54 -0400

    Actualización de HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,6 +2,6 @@ package holamundo;
 
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola mundo XDDD");
+        System.out.println("Hola mundo XDDDD, PROBANDO COMMIT");
     }
 }
