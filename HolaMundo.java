da846c4efc09e42c23f971e1b87a7b031a5925a2
crist0balact <cr.cisternast@duocuc.cl>
2025-05-20 12:23:22 -0400

    Actualización de HolaMundo.java

--- HolaMundo.java
+++ HolaMundo.java
@@ -2,6 +2,6 @@ package holamundo;
 
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola mundo XDDDD, PROBANDO COMMIT");
+        System.out.println("Hola mundo XDDDD, PROBANDO COMMIT, no entiendo aun como se hace commit, debio actualizarse el proyecto en github desde aqui??");
     }
 }
